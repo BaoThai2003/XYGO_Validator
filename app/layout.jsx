2ad6e51efb6169902012b67db9011b587018f5a6
BaoThai2003 <vuonghuynhthaibao.2003@gmail.com>
2026-06-11 18:56:59 +0700

    20. fix layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export const metadata = {
   title: "Xóm YGO Tournament Deck Validator",
   description:
-    "Kiểm tra Deck Condition cho các Archetype trong giải đấu thứ 36 của XYGO",
+    "Kiểm tra Deck Condition của các Archetype trong giải đấu thứ 36 của XYGO",
 };
 
 export default function RootLayout({ children }) {
